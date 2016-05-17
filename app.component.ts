19075219c5a52eeb86f3209b2d51df33f7c6271e
Srini <sbommanaboina1@users.noreply.github.com>
2016-05-17 05:09:25 -0500

    Add files via upload
    form submission

--- app.component.ts
+++ app.component.ts
@@ -4,10 +4,13 @@ import {Component} from 'angular2/core'
 import {Tabs} from './tabs';
 import {Tab} from './tab';
 import {Validators, FormBuilder,Control} from 'angular2/common';
+import {TemplateDrivenForm} from './TemplateDrivenForm';
+import 'rxjs/Rx' ;
+
 @Component({
   selector: 'my-app',
   templateUrl:'./app/clientsettings.html',
- directives: [Tabs, Tab]
+ directives: [Tabs, Tab, TemplateDrivenForm]
 })
 export class AppComponent {
   constructor() {
